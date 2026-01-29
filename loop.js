3472c8477d6bbfa3d43b460c630b8fa6b40c31a0
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-29 13:40:07 +0600

    For and while loop

---
+++ loop.js
@@ -0,0 +1,18 @@
+// console.log("Love you");
+
+// loop: for loop, while loop, do while loop
+
+// for loop
+// for(let i = 0; i<=50; i++){
+//     console.log(i, "I Love You");
+// }
+
+
+let i = 0;
+while(i<=50){
+    console.log(i, "My Name is Khan");
+    i++;
+
+}
+
+
