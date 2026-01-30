2ad5c8229a5b1e9219aff9d625b849672433fe06
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-30 10:27:23 +0600

    Start String basic, And See about access specific character by index, covert captial and small, trim, and slice method

---
+++ string.js
@@ -0,0 +1,28 @@
+let str = "Programming Hero";
+
+// size check
+console.log(str.length);
+
+// access specific character by index
+console.log(str[2]);
+
+// compare two string
+
+let str2 = "Programming hero";
+console.log(str === str2);
+
+// convert to capital or small
+
+console.log(str.toLowerCase() === str2.toLowerCase());
+console.log(str.toUpperCase() === str2.toUpperCase());
+
+// trim: to remove space from beginning to end
+
+let str3 = " Programming Hero";
+console.log(str3.trim());
+
+
+// slice
+let bd = "I Love Bangladesh";
+console.log(bd.slice(2,6));
+
